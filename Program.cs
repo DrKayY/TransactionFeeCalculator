dbc23c2c9383fc221a4a63e3f1e7590ff440a3e4
Yusuff Bello K <ykbello2011@gmail.com>
2020-07-13 13:31:54 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TransFeeCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var feeCalculator = new TransFeeCalculator();
+                var input = 0;
+
+                Console.WriteLine("Enter amount to be transfered:\n");
+
+                if (int.TryParse(Console.ReadLine(), out input))
+                    Console.WriteLine("\nAmount charged:\t{0}\n", feeCalculator.CalculateFee(input));
+                else 
+                {
+                    Console.WriteLine("Invalid input");
+                    break;
+                }
+            }
+
+            Console.WriteLine("\nPress \'ctrl + c\' to exit");
+            Console.ReadLine();
+        }
+    }
+}
